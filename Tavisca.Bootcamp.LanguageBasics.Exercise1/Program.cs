c00b2a95adee8e352ee6a65b86d5d613c2fee6d9
tavisca-dgupta <51922254+tavisca-dgupta@users.noreply.github.com>
2019-06-21 10:47:56 +0530

    Update Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -23,7 +23,46 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int FindDigit(string equation)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            double result=0.0;
+            int index=0;
+            string tempString="";
+            string[] subEq=Regex.Split(equation,"=");
+            subEq[0]=subEq[0].Replace('*','=');
+            string[] substr=Regex.Split(subEq[0],"=");
+            
+            if (subEq[0].Contains("?"))
+            {
+                if(substr[0].Contains("?"))
+                {
+                    index=substr[0].IndexOf('?');
+                    result= Convert.ToDouble(double.Parse(subEq[1])/double.Parse(substr[1]));
+                    tempString=substr[0];
+                }
+                else if(substr[1].Contains("?"))
+                {
+                    index=substr[1].IndexOf('?');
+                    result= Convert.ToDouble(double.Parse(subEq[1])/double.Parse(substr[0]));
+                    tempString=substr[1];
+
+                }
+            }
+            else
+            {
+                index=subEq[1].IndexOf('?');
+                result= Convert.ToDouble(double.Parse(substr[1])*double.Parse(substr[0]));
+                tempString=subEq[1];
+            }
+            if (result%1!=0||tempString.Contains(result.ToString()))
+            {
+                return -1;
+            }
+            else
+            {
+                char result_string=((int)result).ToString()[index];
+                return int.Parse(result_string.ToString());
+            }
+
+            //throw new NotImplementedException();
         }
     }
 }
